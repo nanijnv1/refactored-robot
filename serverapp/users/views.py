cce67a71c5dbbaf3bc8b7c7973a1f5df1ff8a210
nanijnv1 <nani.laxminarayana204@gmail.com>
2021-07-18 19:46:19 +0530

    token validation

--- serverapp/users/views.py
+++ serverapp/users/views.py
@@ -1,9 +1,11 @@
 from sqlalchemy.orm import Session
 import bcrypt
 from .schemas import UserCreateSchema, UserSchema, UserLoginSchema
+from fastapi import HTTPException
 from .models import User, AuthToken
 from datetime import timedelta, datetime
 import jwt
+from sqlalchemy.sql.expression import exists
 
 
 def get_user(db: Session, user_id: int):
@@ -12,6 +14,27 @@ def get_user(db: Session, user_id: int):
 
 def get_user_by_email(db: Session, email: str):
     return db.query(User).filter(User.email == email).first()
+
+
+def validate_token(db: Session, token: str):
+    algorithm = "HS256"
+    jwt_options = {
+        'verify_signature': True,
+        'verify_exp': True,
+        'verify_nbf': False,
+        'verify_iat': True,
+        'verify_aud': False
+    }
+    secret_key = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+    try:
+        decode_token = jwt.decode(token, secret_key, algorithms=[algorithm], options=jwt_options)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail='session expired due to {} or Invalid token'.format(str(e)))
+    token = db.query(AuthToken).filter(AuthToken.token == token, AuthToken.user_id == decode_token['id'])
+    if db.query(token.exists()):
+        return True
+    else:
+        raise HTTPException(status_code=400, detail="session expired login again")
 
 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
@@ -40,9 +63,7 @@ def create_access_token(*, data: dict, expires_delta: timedelta = None, db: Sess
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
-    else:
-        expire = -1
-    to_encode.update({"exp": expire})
+        to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, secret_key, algorithm=algorithm)
     add_token_to_db(db, encoded_jwt, user)
     return encoded_jwt
